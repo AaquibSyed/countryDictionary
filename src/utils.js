a2e48fb69b7807a8971b46790a8aaf80d40c52a5
AaquibSyed <m.nsyedaaquib@gmail.com>
2021-02-25 18:50:02 +0530

    basic table with data done

---
+++ src/utils.js
@@ -0,0 +1,31 @@
+export const COLUMNS = [
+  {
+    Header: "Name",
+    accessor: "name",
+  },
+  {
+    Header: "Capital",
+    accessor: "capital",
+  },
+  {
+    Header: "Population",
+    accessor: "population",
+  },
+
+  {
+    Header: "Region",
+    accessor: "region",
+  },
+  {
+    Header: "Subregion",
+    accessor: "subregion",
+  },
+  {
+    Header: "Timezone",
+    accessor: "timezones[0]",
+  },
+  {
+    Header: "Currrency",
+    accessor: "currencies[0]",
+  },
+];
